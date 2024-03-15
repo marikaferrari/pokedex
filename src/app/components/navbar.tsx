f066defb8c8fa6aeecabac958967f4bd9cea7535
Marika <ferrari.marika@gmail.com>
2024-03-15 09:34:42 +0000

    Adjusted navbar component

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import logo from '../pokedex.png';
+// Logo image
+import logo from '../assets/pokedex.png';
 
 const Navbar = () => {
   return (
     <nav className="border-gray-200 bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2 max-h-20">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Image src={logo} height={120} width={120} alt="pokedex logo" />
+          <Image src={logo} height={130} width={130} alt="pokedex logo" />
         </Link>
       </div>
     </nav>
